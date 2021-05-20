9d13795832e181141264b042a8ef9790338c315d
dshulgaster <dshulgaster@gmail.com>
2021-05-20 19:53:55 +0300

    Lesson 01 (не добавлен CalendarView, эксперименты продолжаются)

---
+++ app/src/main/java/gb/android_1/les01/SecondActivity.java
@@ -0,0 +1,17 @@
+package gb.android_1.les01;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
